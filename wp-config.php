7fe0158dafe8a35ccdeb6707640b78c38ac7dd21
Samuel Oliveira <11sosam11@gmail.com>
2022-07-19 15:18:23 -0500

    testing

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@
 /** The name of the database for WordPress */
 if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
     $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
-    define('DB_NAME', trim($db[`path`],`/`));
-    define('DB_USER', $db[`user`]);
-    define('DB_PASSWORD', $db[`pass`]);
-    define('DB_HOST', $db[`host`]);
+    define('DB_NAME', trim($db['path'],`/`));
+    define('DB_USER', $db['user']);
+    define('DB_PASSWORD', $db['pass']);
+    define('DB_HOST', $db['host']);
     define('DB_CHARSET', 'utf8');
     define('DB_COLLATE', '');
 } else {
